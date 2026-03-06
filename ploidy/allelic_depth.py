cb22f2d82c20ed61492d0b7631b2fd4d11486f5e
Ellen McMullen <72088579+EllenMcMullen@users.noreply.github.com>
2026-03-05 22:14:06 -0800

    dif depths

--- ploidy/allelic_depth.py
+++ ploidy/allelic_depth.py
@@ -5,12 +5,11 @@
 
 # in r console
 #install.packages("reticulate")
-#library(reticulate)
-#use_condaenv(phha) # name of conda env
-#reticulate::py_config() # check that it is the right one
+library(reticulate)
+use_condaenv('phha') # name of conda env
+reticulate::py_config() # check that it is the right one
 
 # in terminal copy over the current vcf and the vcf with the 2 removed pops
-
 
 # running first command also runs reticulate::repl_python() so you can use python in rstudio
 # exit will return to r
@@ -88,24 +87,133 @@ for variant in vcf:
 df = pd.DataFrame(allele_data)    
 df.head()
 df['allele_fraction'] = [x[0] if not np.isnan(x) else np.nan for x in df['allele_fraction']]
+df['depth'] = [x[0] if not np.isnan(x) else np.nan for x in df['depth']]
 df.head()
 df.to_csv(csv_file, index=False)
 
-min_depth = 1
-plot_file = f'allele_fraction_by_population_{vers}_min_dp_{min_depth}.png'
 # aggregate by population and plot
 pops = df['population'].unique()
 pops
 
-df2 = df
+plt.figure()
+sns.histplot(df['depth'], bins = 50)
+plt.show()
 
+# try 1, 4, 8, 10
+min_depth = 4
+
+d = df['depth']>= min_depth
+df2 = df.loc[d, :]
+
+
+# overall hist
+plt.figure()
+sns.histplot(df['allele_fraction'], bins=50)
+plt.vlines([0, 0.25, 0.5, 0.75, 1], 0, 700, 'k')
+plt.show()
+
+# overall density
+plt.figure()
+sns.kdeplot(df['allele_fraction'], fill=True, color='purple', alpha=0.5, bw_adjust=0.5)
+plt.vlines([0, 0.25, 0.5, 0.75, 1], 0, 3, 'k')
+plt.show()
+
+# plot each pop separately hist
 for p in pops:
   in_p = df2['population'] == p
   pdf = df2[in_p.values]
-  pop_dfs.append(pdf)
   plt.figure()
-  sns.kdeplot(pdf['allele_fraction'], fill=True, color='purple', alpha=0.5)
+  sns.histplot(pdf['allele_fraction'], bins = 50)
   plt.vlines([0, 0.25, 0.5, 0.75, 1], 0, 2, 'k')
   plt.title(f'{p}')
+  #plt.savefig(f'allele_fraction_pop_{p}_{vers}_min_dp_{min_depth}.png')
   plt.show()
-  plt.savefig(f'allele_fraction_pop_{p}_{vers}_min_dp_{min_depth}.png')
+
+# plot each pop separately density
+for p in pops:
+  in_p = df2['population'] == p
+  pdf = df2[in_p.values]
+  plt.figure()
+  sns.kdeplot(pdf['allele_fraction'], fill=True, color='purple', alpha=0.5, bw_adjust=0.4)
+  plt.vlines([0, 0.25, 0.5, 0.75, 1], 0, 2, 'k')
+  plt.title(f'{p}')
+  #plt.savefig(f'hist_allele_fraction_pop_{p}_{vers}_min_dp_{min_depth}.png')
+  plt.show()
+  
+# plot all pops together
+plt.figure()
+sns.kdeplot(data=df2, x="allele_fraction", hue="population", fill=True, alpha=0.3, bw_adjust=0.4)
+plt.vlines([0,0.25,0.5,0.75,1], 0, 0.2, 'k')
+##plt.savefig(f'allele_fraction_all_pops_{vers}_min_dp_{min_depth}.png')
+plt.show()
+
+# plot all pops together
+plt.figure()
+sns.histplot(data=df2, x="allele_fraction", hue="population", fill=True, alpha=0.3, bins = 50)
+plt.vlines([0,0.25,0.5,0.75,1], 0, 0.2, 'k')
+#plt.savefig(f'hist_allele_fraction_all_pops_{vers}_min_dp_{min_depth}.png')
+plt.show()
+
+# plot pop per panel
+g = sns.displot(
+    data=df2,
+    x="allele_fraction",
+    col="population",
+    col_wrap=4,          # number of panels per row
+    kind="kde",
+    fill=True,
+    height=3,
+    aspect=1.2,
+    bw_adjust=0.35
+)
+#for ax in g.axes.flat:
+#    ax.vlines([0, 0.25, 0.5, 0.75, 1], 0, 0.15, 'k')
+g.fig.subplots_adjust(top=0.9)
+g.fig.suptitle(f"Allele fraction distribution by population (min depth {min_depth})")
+#plt.savefig(f'allele_fraction_by_population_panels_{vers}_min_dp_{min_depth}.png')
+plt.show()
+
+
+# plot pop per panel
+g = sns.displot(
+    data=df2,
+    x="allele_fraction",
+    col="population",
+    col_wrap=4,          # number of panels per row
+    kind="hist",
+    fill=True,
+    height=3,
+    aspect=1.2,
+    bins = 30
+)
+#for ax in g.axes.flat:
+#    ax.vlines([0, 0.25, 0.5, 0.75, 1], 0, 8, 'k')
+g.fig.subplots_adjust(top=0.9)
+g.fig.suptitle(f"Allele fraction distribution by population (min depth {min_depth})")
+##plt.savefig(f'hist_allele_fraction_by_population_panels_{vers}_min_dp_{min_depth}.png')
+plt.show()
+
+
+
+plt.close('all')
+
+for min_depth in [1, 2, 4, 6, 8, 10]:
+  d = df['depth']>= min_depth
+  df2 = df.loc[d, :]
+  g = sns.displot(
+      data=df2,
+      x="allele_fraction",
+      col="population",
+      col_wrap=4,
+      kind="kde",
+      fill=True,
+      height=3,
+      aspect=1.2,
+      bw_adjust=0.35
+  )
+  g.fig.subplots_adjust(top=0.9)
+  g.fig.suptitle(f"Allele fraction distribution by population (min depth {min_depth})")
+  plt.savefig(f'allele_fraction_by_population_panels_{vers}_min_dp_{min_depth}.png')
+  plt.show()
+
+
